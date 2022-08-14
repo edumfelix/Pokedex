06f269a9091d7d132225d5f92918ee6a53b490e9
Eduardo Felix <dudumacedofelix613@gmail.com>
2022-08-14 15:50:58 -0300

    consertando casos de nao existencia

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,13 @@ const pokemonImage = document.querySelector('.pokemon_image');
 
 const form = document.querySelector('.form');
 const input = document.querySelector('.input_search');
+const buttonPrev = document.querySelector('.btn-prev');
+const buttonNext = document.querySelector('.btn-next');
+
+let searchPokemon = 1;
 
 const fetchPokemon = async (pokemon) => {
-  const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`);
+  const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
   
   if (APIResponse.status === 200) {
     const data = await APIResponse.json();
@@ -15,6 +19,8 @@ const fetchPokemon = async (pokemon) => {
 }
 
 const renderPokemon = async (pokemon) => {
+  pokemonName.innerHTML = 'Loading...';
+  pokemonNumber.innerHTML = '';
   const data = await fetchPokemon(pokemon);
   
   if (data) {
@@ -22,11 +28,24 @@ const renderPokemon = async (pokemon) => {
     pokemonNumber.innerHTML = data.id;
     pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
     input.value = '';
+  } else {
+    pokemonImage.style.display = 'none';
+    pokemonName.innerHTML = 'Not Found!';
+    input.value = '';
   }
 }
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
-
-  renderPokemon(input.value);
+  renderPokemon(input.value.toLowerCase());
 });
+
+buttonPrev.addEventListener('click', (event) => {
+  alert('prev clicked');
+});
+
+buttonNext.addEventListener('click', (event) => {
+  alert('next clicked');
+});
+
+renderPokemon(searchPokemon);
